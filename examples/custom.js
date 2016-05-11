e35f9fe33f17f352373549fec0bf23071a525fac
Zak3Mak <Zak3Mak@gmail.com>
2016-05-11 13:46:50 -0700

    Update custom.js

--- examples/custom.js
+++ examples/custom.js
@@ -3,7 +3,7 @@
 // - custom info window (using Info Bubble)
 // - custom info window content (+ store hours)
 
-var ICON = new google.maps.MarkerImage('medicare.png', null, null,
+var ICON = new google.maps.MarkerImage('HD.png', null, null,
     new google.maps.Point(14, 13));
 
 var SHADOW = new google.maps.MarkerImage('medicare-shadow.png', null, null,
@@ -11,7 +11,7 @@ var SHADOW = new google.maps.MarkerImage('medicare-shadow.png', null, null,
 
 google.maps.event.addDomListener(window, 'load', function() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(-28, 135),
+    center: new google.maps.LatLng(-117, 33),
     zoom: 4,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
