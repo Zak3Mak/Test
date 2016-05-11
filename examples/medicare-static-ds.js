6111902077a60bb43abe36d6ca7e8869bd2216a8
Zak3Mak <Zak3Mak@gmail.com>
2016-05-11 13:09:57 -0700

    Update medicare-static-ds.js

--- examples/medicare-static-ds.js
+++ examples/medicare-static-ds.js
@@ -17,7 +17,7 @@ function MedicareDataSource() {
  * @private
  */
 MedicareDataSource.prototype.FEATURES_ = new storeLocator.FeatureSet(
-  new storeLocator.Feature('Wheelchair-YES', 'Wheelchair access'),
+  new storeLocator.Feature('SuperCenter-YES', 'SuperCenter'),
   new storeLocator.Feature('Audio-YES', 'Audio')
 );
 
@@ -41,7 +41,7 @@ MedicareDataSource.prototype.parse_ = function(csv) {
   for (var i = 1, row; row = rows[i]; i++) {
     row = this.toObject_(headings, this.parseRow_(row));
     var features = new storeLocator.FeatureSet;
-    features.add(this.FEATURES_.getById('Wheelchair-' + row.Wheelchair));
+    features.add(this.FEATURES_.getById('SuperCenter-' + row.SuperCenter));
     features.add(this.FEATURES_.getById('Audio-' + row.Audio));
 
     var position = new google.maps.LatLng(row.Ycoord, row.Xcoord);
