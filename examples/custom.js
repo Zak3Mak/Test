dde557df42fb33e2c30fb7311348b6c461baf3f7
Zak3Mak <Zak3Mak@gmail.com>
2016-05-11 13:36:22 -0700

    Update custom.js

--- examples/custom.js
+++ examples/custom.js
@@ -11,7 +11,7 @@ var SHADOW = new google.maps.MarkerImage('medicare-shadow.png', null, null,
 
 google.maps.event.addDomListener(window, 'load', function() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(111, -35),
+    center: new google.maps.LatLng(-28, 135),
     zoom: 6,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
