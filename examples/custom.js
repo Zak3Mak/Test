4c75d8823c6c0ce43f2f9ced2aa7c14e2085c71f
Zak3Mak <Zak3Mak@gmail.com>
2016-05-12 07:43:05 -0700

    Update custom.js

--- examples/custom.js
+++ examples/custom.js
@@ -11,8 +11,8 @@ var ICON = new google.maps.MarkerImage('Behr.png', null, null,
 
 google.maps.event.addDomListener(window, 'load', function() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(-28, 135),
-    zoom: 4,
+    center: new google.maps.LatLng(38, -96),
+    zoom: 5,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
