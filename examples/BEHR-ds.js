b8243b26d3c6172d239cdb861537a6c200e48a8d
Zak3Mak <Zak3Mak@gmail.com>
2016-05-12 15:17:40 -0700

    Update BEHR-ds.js

--- examples/BEHR-ds.js
+++ examples/BEHR-ds.js
@@ -46,11 +46,13 @@ BEHRDataSource.prototype.parse_ = function(csv) {
 
     var position = new google.maps.LatLng(row.Ycoord, row.Xcoord);
 
-    var shop = this.join_([row.Shp_num_an, row.Shp_centre], ', ');
+    //var shop = this.join_([row.Shp_num_an, row.Shp_centre], ', ');
+    var shop = this.join_([row.Shp_num_an], ', ');
     var locality = this.join_([row.Locality, row.Postcode], ', ');
 
     var store = new storeLocator.Store(row.uuid, position, features, {
       title: row.Fcilty_nam,
+      phone: row.Shp_centre,
       address: this.join_([shop, row.Street_add, locality], '<br>'),
       hours: row.Hrs_of_bus
     });
